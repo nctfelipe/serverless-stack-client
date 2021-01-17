6c5cbf66734f9ff51e1007225b399e6b725532c2
Felipe Nascimento <nctfelipe@gmail.com>
2021-01-17 19:09:30 -0300

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+    s3: {
+        REGION: "",
+        BUCKET: "",
+    },
+    apiGateway: {
+        REGION: "",
+        URL: "",
+    },
+    cognito: {
+        REGION: "",
+        USER_POOL_ID: "",
+        APP_CLIENT_ID: "",
+        IDENTITY_POOL_ID: "",
+    },
+};
+
+export default config;
